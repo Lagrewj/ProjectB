1d7599f9cab46dbe0a8ac0ce85a827700b64713e
Tim Robinson <robinsti@engr.orst.edu>
2015-12-04 12:48:34 -0800

    Initial deposit upload

---
+++ Webpages/deposit.php
@@ -0,0 +1,44 @@
+<?php
+	ini_set('display_errors', 'On');
+	session_start();
+
+	if(!(isset($_SESSION['logged_in_status']))) {
+		header('Location: ./signin.php');
+		exit;
+	}
+
+	require "./db_connect.php";
+	require "./navigation.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Deposit Money</title>
+
+		<link rel="stylesheet" href="stylesheets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="stylesheets/bootstrap/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="stylesheets/bootstrap/css/docs.min.css">
+
+		<script src="stylesheets/bootstrap/js/jquery-1.11.0.min.js"></script>
+		<script src="stylesheets/bootstrap/js/bootstrap.min.js"></script>	
+		<script type="text/javascript" src="donate_script.js"></script>
+	</head>
+	<body>
+	<div class="container">
+		<?php
+			echo $navbar; ?>
+
+			<h1 class="pageHeader"><?php echo $_SESSION['first_name']; ?>'s Deposit Page</br></br></h1>
+			<h3> Current Balance: $<?php echo $_SESSION['balance']; ?><br></br></h3>
+			<form method="post" action="deposit_transaction.php">
+				<fieldset>
+					<legend>Deposit Amount:</legend>
+					$<input type="number" placeholder="0.00" name="deposit" /><br><br><br>
+					<input type="submit" value="Deposit" />
+				</fieldset>
+			</form>
+	</body>
+</html>
---
+++ Webpages/deposit_transaction.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+
+	ini_set('display_errors', 'On');
+
+	// if(!isset($_SESSION['logged_in_status'])) {
+	// 	header("Location: signin.php");
+	// 	exit();
+	// }
+
+ 	require "./db_connect.php";
+ 	require "./navigation.php";
+
+// 	echo $navbar;
+
+echo "Session email: " . $_SESSION['email_address'];
+if(!$mysqli || $mysqli->connect_errno){
+ 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}	
+
+ 	if(!($stmt = $mysqli->prepare("UPDATE `usr_db` SET `balance`=`balance` + ? WHERE `email_address` = ?"))) {
+ 		echo "Prepare failed: " . $stmt->errno . " " . $stmt->error;
+ 	}
+
+ 	if(!($stmt->bind_param("is", $_POST['deposit'], $_SESSION['email_address']))) {
+ 		echo "Bind failed: " . $stmt->errno . " " . $stmt->error;
+ 	}
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Successful Deposit!";	
+}
+
+$_SESSION['balance'] = $_SESSION['balance'] + $_POST['deposit'];
+?>
