2096ecfbdd86acd191d3eeb5f41144702b8f3ddf
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-12-06 12:47:40 -0800

    Create test_donate.php

---
+++ Webpages/test_donate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Test Donation</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=2.0">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="qunit/qunit/qunit.css">
+        
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="jQuery-SlotMachine/dist/jquery.slotmachine.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+
+        <!-- QUnit Testing -->
+        <script type="text/javascript" src="qunit/qunit/qunit.js"></script>
+        <script type="text/javascript" src="test_slots.js"></script>
+    </head>
+
+    <body>
+        <div id="qunit"></div>
+        <div id="qunit-fixture"></div>
+
+<?php
+    require "./donate.php";
+?>
